c47b6ef4bfadf61a6ee332e5ba4a757a28dc4811
Suraj r <surajraghunath.2@gmail.com>
2025-08-22 20:10:55 +0530

    Delete bapp directory

--- bapp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class BappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bapp'
-def ready(self):
-    import bapp.signals
